9142a2e302ae3fad845af68ca51632ffab031fac
Emil Lundberg <emil@emlun.se>
2021-10-31 19:02:24 +0100

    Simplify MapDef definition

--- src/types/map.ts
+++ src/types/map.ts
@@ -19,19 +19,5 @@ export const floorNums = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1] as 
 export type FloorNum = (typeof floorNums)[number];
 
 export type MapDef = {
-  1: RoomDef[],
-  2: RoomDef[],
-  3: RoomDef[],
-  4: RoomDef[],
-  5: RoomDef[],
-  6: RoomDef[],
-  7: RoomDef[],
-  8: RoomDef[],
-  9: RoomDef[],
-  10: RoomDef[],
-  11: RoomDef[],
-  12: RoomDef[],
-  13: RoomDef[],
-  14: RoomDef[],
-  15: RoomDef[],
+  [f in FloorNum]: RoomDef[];
 }
